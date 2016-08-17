5d997bb83ff0d0ebd6e57fe1ee453da4d1926e7f
Craig.C.Li <Craig.C.Li@newegg.com>
2016-08-17 21:12:01 +0800

    add user post api

--- src/controller/user_controller.py
+++ src/controller/user_controller.py
@@ -8,20 +8,24 @@ from src.entity.user import User
 from src import db
 
 
-@app.route('/medical-case-of-illness/user/<user_id>',methods=['GET'])
-def get_user_by_id(user_id):
-    user = User.query.filter_by(user_id=user_id).first()
-    dit = user.get_dict()
-    ret = flask.Response(json.dumps(dit))
+@app.route('/medical-case-of-illness/user/<user_name>',methods=['GET'])
+def get_user_by_id(user_name):
+    user = User.query.filter_by(name=user_name).all()
+    lit =[]
+    for temp_user in user:
+        lit.append(temp_user.get_dict())
+    ret = flask.Response(json.dumps(lit))
     ret.headers['Access-Control-Allow-Origin'] = '*'
     return ret
 @app.route('/medical-case-of-illness/user/',methods = ['POST'])
 def add_user():
-    if request.form['name']!=None:
+    if request.form['name'] is not None and request.form['name']!= '' and request.form['contact']!=None:
         current_user = _form_to_user(request.form)
         db.session.add(current_user)
         db.session.commit()
-        ret = flask.Response()
+        response_user = User.query.filter_by(name=request.form['name']).first()
+        result = response_user.get_dict()
+        ret = flask.Response(json.dumps(result))
         ret.headers['Access-Control-Allow-Origin']='*'
         return ret,httplib.OK
     else:
