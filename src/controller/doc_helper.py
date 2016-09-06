b6b21918428be46798a9be895b418a4bff4feed6
Craig.C.Li <Craig.C.Li@newegg.com>
2016-09-06 22:16:47 +0800

    add doc helper

--- src/controller/doc_helper.py
+++ src/controller/doc_helper.py
@@ -4,9 +4,13 @@ import sys
 from docx import Document
 from src import app
 from src.controller.common_function import check_directory, check_file
+from src.entity.diagnose import Diagnose
+from src.entity.difficulty_assessment import Difficulty_assessment
 from src.entity.illness_history import Illness_history
+from src.entity.non_surgical import Non_surgical
 from src.entity.oral_examination import Oral_examination
 from src.entity.personal_history import Personal_history
+from src.entity.surgical import Surgical
 from src.entity.tooth_location import Tooth_location
 from src.entity.user import User
 
@@ -43,8 +47,20 @@ def _get_dictionary(tooth_id):
     illness_history = Illness_history.query.filter_by(tooth_id=tooth_id).first()
     personal_history = Personal_history.query.filter_by(user_id=user_id).first()
     oral_examination = Oral_examination.query.filter_by(tooth_id=tooth_id).first()
+    diagnose = Diagnose.query.filter_by(tooth_id=tooth_id).first()
+    difficulty_assessment = Difficulty_assessment.query.filter_by(tooth_id=tooth_id).first()
+    surgical = Surgical.query.filter_by(tooth_id=tooth_id).first()
+    if not surgical:
+        surgical = Non_surgical.query.filter_by(tooth_id=tooth_id).first()
+    oral_tooth_location = oral_examination.tooth_location
+    oral_examination_dict = oral_examination.get_dict()
+    del oral_examination_dict['tooth_location']
     full_dict = dict(tooth_location.get_dict().items() + user.get_dict().items() + illness_history.get_dict().items()
-                     + personal_history.get_dict().items() + oral_examination.get_dict().items())
+                     + personal_history.get_dict().items() + oral_examination_dict.items() +
+                     diagnose.get_dict().items() +surgical.get_dict().items()+
+                     difficulty_assessment.get_dict().items())
+    full_dict['oral_tooth_location'] = oral_tooth_location
+    full_dict['tooth_location'] = tooth_location.tooth_location
     for key, value in full_dict.items():
         if value == '是':
             full_dict[key] = '有'
@@ -75,6 +91,13 @@ def _get_dictionary(tooth_id):
                                                          full_dict['is_medicine'],
                                                          full_dict['medicine_name'],
                                                          full_dict['is_relief'])
+    if full_dict['handle_type']==1:
+        if not full_dict['appease_medicine']=='':
+            full_dict['appease_medicine'] = '安抚药物{0},'.format(full_dict['appease_medicine'])
+            full_dict['observed_time'] = '观察时间{0}。'.format(full_dict['observed_time'])
+        if not full_dict['modulo']=='':
+            full_dict['modulo'] = '取模材料{0}。'.format(full_dict['modulo'])
+            full_dict['inlay'] = '嵌体材料{0}。'.format(full_dict['inlay'])
     if full_dict['gender'] == True:
         full_dict['gender'] = '女'
     else:
