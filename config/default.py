cac80ea6c530cea94f9a4947d7524009a9f9b51e
Craig.C.Li <Craig.C.Li@newegg.com>
2016-09-27 17:20:44 +0800

    fix bug

--- config/default.py
+++ config/default.py
@@ -7,7 +7,7 @@ LOG = 'var/medical_case_of_illness/logs'
 
 QUERY_LIMIT = 10
 PER_PAGE = 10
-ALLOWED_EXTENSIONS=set(['txt','pdf','png','jpg','jpeg','gif'])
+ALLOWED_EXTENSIONS=set(['txt','pdf','png','jpg','jpeg','gif','JPG','PNG'])
 TEMPLETE_LOCATION ='C:\Users\solitaire\Desktop\illness.docx'
 
 STATIC_FILES_PATH='D:\Medical_Case\\'
